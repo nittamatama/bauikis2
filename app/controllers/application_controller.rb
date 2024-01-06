40f022f23b7a8a4ecfa9cbb3f2e323ed06e196d6
nittamatama <blackmnshin@gmail.com>
2024-01-06 10:26:17 +0000

    403用のエラーページ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  # 例外ハンドラー
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
 
   private
 
@@ -11,6 +17,10 @@ class ApplicationController < ActionController::Base
       else
         "customer"
       end
+    end
+
+    def rescue403(e)
+      render "errors/forbidden", status: 403
     end
 
     def rescue500(e)
